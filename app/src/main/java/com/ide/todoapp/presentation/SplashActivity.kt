d01f28ea5e2578d687042a61743e33c64ab2b59c
shefo159 <99135774+shefo159@users.noreply.github.com>
2022-02-08 13:52:56 +0200

    splash screen by mahmoud sharaf

--- app/src/main/java/com/ide/todoapp/presentation/SplashActivity.kt
+++ app/src/main/java/com/ide/todoapp/presentation/SplashActivity.kt
@@ -1,12 +1,22 @@
 package com.ide.todoapp.presentation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import com.ide.todoapp.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        val imageView : ImageView =findViewById(R.id.iv_note)
+        imageView.alpha=0f
+        imageView.animate().setDuration(1500).alpha(1f).withEndAction{
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            finish()
+        }
     }
 }
